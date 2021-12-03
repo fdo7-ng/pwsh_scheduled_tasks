45962f3996faae8c4ce937591592236a679dc510
fernando.ng <fernando.ng@fnf.com>
2021-12-03 21:37:38 +0000

    initial upload

---
+++ scheduledtask_schema.ps1
@@ -0,0 +1,72 @@
+﻿$task_schema = @"
+<?xml version="1.0" encoding="UTF-16"?>
+<Task version="1.2" xmlns="http://schemas.microsoft.com/windows/2004/02/mit/task">
+  <Triggers>
+    <CalendarTrigger>
+      <StartBoundary>2021-12-05T05:00:00+00:000</StartBoundary>
+      <Enabled>true</Enabled>
+      <ScheduleByMonthDayOfWeek>
+        <Weeks>
+          <Week>1</Week>
+          <Week>2</Week>
+          <Week>3</Week>
+        </Weeks>
+        <DaysOfWeek>
+          <Sunday />
+        </DaysOfWeek>
+        <Months>
+          <January />
+          <February />
+          <March />
+          <April />
+          <May />
+          <June />
+          <July />
+          <August />
+          <September />
+          <October />
+          <November />
+          <December />
+        </Months>
+      </ScheduleByMonthDayOfWeek>
+    </CalendarTrigger>
+  </Triggers>
+  <Principals>
+    <Principal id="Author">
+      <UserId>S-1-5-18</UserId>
+      <RunLevel>HighestAvailable</RunLevel>
+    </Principal>
+  </Principals>
+  <Settings>
+    <MultipleInstancesPolicy>IgnoreNew</MultipleInstancesPolicy>
+    <DisallowStartIfOnBatteries>true</DisallowStartIfOnBatteries>
+    <StopIfGoingOnBatteries>true</StopIfGoingOnBatteries>
+    <AllowHardTerminate>true</AllowHardTerminate>
+    <StartWhenAvailable>false</StartWhenAvailable>
+    <RunOnlyIfNetworkAvailable>false</RunOnlyIfNetworkAvailable>
+    <IdleSettings>
+      <StopOnIdleEnd>true</StopOnIdleEnd>
+      <RestartOnIdle>false</RestartOnIdle>
+    </IdleSettings>
+    <AllowStartOnDemand>true</AllowStartOnDemand>
+    <Enabled>true</Enabled>
+    <Hidden>false</Hidden>
+    <RunOnlyIfIdle>false</RunOnlyIfIdle>
+    <WakeToRun>false</WakeToRun>
+    <ExecutionTimeLimit>PT72H</ExecutionTimeLimit>
+    <Priority>7</Priority>
+  </Settings>
+  <Actions Context="Author">
+    <Exec>
+      <Command>C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe</Command>
+      <Arguments>-NoProfile -WindowStyle Hidden -command "&amp; { Install-Module PSWindowsUpdate -Force; Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -AutoReboot }"</Arguments>
+    </Exec>
+  </Actions>
+</Task>
+"@
+
+
+
+Register-ScheduledTask -User 'nt authority\system' -Xml $task_schema -TaskName "automated_os_patching" -Force
+
+
---
+++ schtask.ps1
@@ -0,0 +1,43 @@
+﻿
+$taskParams = @("/Create",
+            "/TN", "AutomatedScheduledTask", 
+            "/SC", "weekly", 
+            "/D", "Sunday", 
+            "/ST", "08:00", 
+            "/TR", "c:\temp\test.ps1", 
+            "/F", #force
+            "/RU", "system");
+
+# supply the command arguments and execute  
+#schtasks.exe $taskParams
+ schtasks.exe @taskParams
+
+
+
+ $callParams = @("/Create", 
+					"/TN", "schtask_taskName_weekly", 
+					"/SC", "weekly", 
+					"/D", "SUN", 
+					"/ST", "08:00", 
+					"/TR", ("powershell.exe -File `"$thisScript`" -ConsoleOutputFile `"$logFile`"" -replace '"', '\"'), 
+					"/F", #force
+					"/RU", "system") #run under the system account
+& "schtasks.exe" $callParams
+
+
+
+
+ $callParams = @("/Create", 
+					"/TN", "schtask_taskName_monthly", 
+					"/SC", "Monthly", 
+					"/M", "*", 
+					"/ST", "08:00", 
+                    "/MO", @("FIRST","THIRD"),
+                    "/D", "SUN",
+					"/TR", ("powershell.exe - `"$thisScript`" -ConsoleOutputFile `"$logFile`"" -replace '"', '\"'), 
+					"/F", #force
+					"/RU", "system") #run under the system account
+& "schtasks.exe" $callParams
+
+
+schtasks /create /tn "My App" /tr "c:\apps\myapp.exe" /sc monthly /mo 1
